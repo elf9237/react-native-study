d3d39923bd02f89a48aecf5f19f2fb3b4b54c88c
e <565071984@qq.com>
2017-09-06 11:01:02 +0800

    配置文件更新

---
+++ imooc_test/组件.js
@@ -0,0 +1,113 @@
+// 卸载组件及第三方依赖包
+react-native unlink react-native-video
+npm uninstall react-native-video --save
+
+esline
+npm i -D eslint babel-eslint eslint-plugin-react
+// 底部导航菜单
+react-native-tab-navigator
+
+// 弹出框提示组件
+react-native-easy-toast
+
+// tab切换组件
+react-native-scrollable-tab-view
+
+// 启动屏
+react-native-splash-screen
+
+// html渲染
+react-native-htmlview
+
+// 列表排序
+react-native-sortable-listview
+
+// 复选框
+react-native-check-box
+
+// 第三方类型检查
+prop-types
+
+// 提示框 popover必须放在视图根目录下
+react-native-popover   
+
+// 用法：
+Greeting.propTypes = {
+    name: PropTypes.string
+  };
+
+// react-native
+AsyncStorage // 本地数据存储
+StatusBar   // 状态栏
+Platform  // 判断运行平台
+RefreshControl // 下拉刷新功能，用在ScrollView或ListView内部
+WebView   //内嵌网页，自带loading效果
+
+// 展示多数据源的几个组件对比
+
+ScrollView 
+// 会把所有子元素一次性全部渲染出来
+// 使用上最简单
+// 但是如果你有一个特别长的列表需要显示,可能会需要好几屏的高度
+// 这时就会占用很大的内存去创建和渲染那些屏幕以外的JS组件和原生视图,性能上也会有所拖累
+ListView 
+// 更适用于长列表数据,它会惰性渲染子元素,并不会立即渲染所有元素
+// 而是优先渲染屏幕上可见的元素
+FlatList 
+//  0.43版本开始新出的改进版的ListView,性能更优
+// 但是官方说现在可能不够稳定,尚待时间考验,但是它不能够分组/类/区(section)
+SectionList
+// 0.43版本推出的,高性能的分组列表组件
+// 但是它不支持头部吸顶悬浮的效果,但是也不要伤心,官方在下一个版本开始就可以支持悬浮的section头部啦
+
+
+
+
+Note
+// 循环数据都要加key
+// 做数据处理时候，没有数据返回null
+
+// 生成数据调用方式
+// 1、利用json数据
+// 2、通过构造函数传入参数，通过对象属性调用
+// 3、生成一个对象，形成键值对（常用于做标识）
+
+// 数据重新封装
+// 建立对象
+export default function ProjectModel(item, isFavorite) {
+  this.item = item;
+  this.isFavorite = isFavorite;
+}
+
+// 调用
+let projectModels = [];
+let items = this.items;
+for(var i =0, len = items.length; i< len; i++) {
+    projectModels.push(new ProjectModel(items[i], false));
+}
+
+
+// 遍历方法
+map
+forEach
+
+JSON.stringify()  // 将对象解析成字符串
+JSON.parse()  // 将字符串解析成对象
+Response.JSON()
+
+// 滚动监听高度
+_contentViewScroll = (e) => {
+  var offsetY = e.nativeEvent.contentOffset.y; //滑动距离
+  var contentSizeHeight = e.nativeEvent.contentSize.height; //scrollView contentSize高度
+  var oriageScrollHeight = e.nativeEvent.layoutMeasurement.height; //scrollView高度
+  if (offsetY + oriageScrollHeight >= contentSizeHeight){
+      console.log('上传滑动到底部事件')
+  }
+}
+
+// setState 方法重写 
+_updateState= (dic) => {
+  if(!this)return;   // 如果组件不存在，返回
+  this.setState({dic});
+}
+
